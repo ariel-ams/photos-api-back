68b22a0bc03ce04b9bdf50a26785d3555a43fd02
Ariel Medina <arielm92@gmail.com>
2021-04-02 01:07:47 -0300

    Improved feedback messages and removed hardcoded test data

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ let auth = (request, response, next) => {
     let token = request.cookies.auth;
     User.findByToken(token, (error, user) => {
         if(error) throw error;
-        if(!user) return response.json({error: true});
+        if(!user) return response.json({error: 'Auth error'});
 
         request.token = token;
         request.user = user;
